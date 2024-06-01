1bd71ce9c02c3e9e73e080ddf4dba3ffe14329b9
schmeekygeek <ss6087982@gmail.com>
2024-06-02 00:21:09 +0530

    added prefixparse

--- ast/ast.go
+++ ast/ast.go
@@ -40,6 +40,24 @@ type ExpressionStatement struct {
 	Expression Expression
 }
 
+type PrefixExpression struct {
+  Token token.Token // The prefix token, e.g. !
+  Operator string
+  Right Expression
+}
+
+func (pe *PrefixExpression) expressionNode()      {}
+func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
+func (pe *PrefixExpression) String() string       {
+  var out bytes.Buffer
+
+  out.WriteString("(")
+  out.WriteString(pe.Operator)
+  out.WriteString(pe.Right.String())
+  out.WriteString(")")
+  return out.String()
+}
+
 type IntegerLiteral struct {
 	Token token.Token
 	Value int64
